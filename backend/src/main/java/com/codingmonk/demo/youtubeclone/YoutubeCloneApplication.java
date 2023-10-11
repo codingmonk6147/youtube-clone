be68446f943b56cb8f98b74d5febdf70f2e53500
Bibhuti Baishya <codingmonk6147.com>
2023-10-11 18:12:35 +0000

    New Setup

---
+++ backend/src/main/java/com/codingmonk/demo/youtubeclone/YoutubeCloneApplication.java
@@ -0,0 +1,13 @@
+package com.codingmonk.demo.youtubeclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeCloneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codingmonk/demo/youtubeclone/YoutubeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingmonk.demo.youtubeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
